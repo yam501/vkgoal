ec0aa0bbdf2400fc9150c6ca7b59f9ce31ea1b62
yam501 <misha_yashin_00@mail.rum>
2022-12-18 11:28:51 +0500

    fix

--- vkgoal/src/panels/Selection.js
+++ vkgoal/src/panels/Selection.js
@@ -1,10 +1,9 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
-<<<<<<< HEAD
+
 import { Panel, PanelHeader, PanelHeaderBack, Group, Button, Div, Text } from '@vkontakte/vkui';
 
-import selection from '../img/persik.png';
 import './Selection.css';
 
 const Selection = (id, go) => {
@@ -13,49 +12,17 @@ const Selection = (id, go) => {
 
 	return (
 		<Panel id={id}>
-			<PanelHeader className='header'>
-				VK GOAL
-			</PanelHeader>
-			<Group className="content">
-				<Div className='content__select_maket'>
-					<div
-					onClick={() => setActive(0)} 
-					className='select_maket_card'>
-						<div className='card__icon'></div>
-						<div className='card__text'>Программирование</div>
-					</div>
-					<div
-					onClick={() => setActive(1)} 
-					className={`select_maket_card ${active === 1 ? "active" : ''}`}>
-						<div className='card__icon'></div>
-						<div className='card__text'>Спорт</div>
-					</div>
-					<div
-					onClick={() => setActive(2)}
-					className='select_maket_card'>
-						<div className='card__icon'></div>
-						<div className='card__text'>Творчество</div>
-					</div>
-				</Div>
-			</Group>
+			<div className='header'>
+				<div> </div>
+				<span>vk goal</span>
+				<div> </div>
+			</div>
+			<div className='header_text'>Самое приятное - достичь своих</div>
+			<div>
+			</div>
 		</Panel>
 	)
 };
-=======
-import { Panel, PanelHeader, Header, Button, Group, Cell, Div, Avatar } from '@vkontakte/vkui';
-
-const Selection = ({ id, go, fetchedUser }) => (
-	<Panel id={id}>
-		<Group header={<Header>Самое приятное - достичь своих целей</Header>}>
-			<Div>
-				<Button stretched size="l" onClick={go} data-to="selection">
-					Поставить цель 
-				</Button>
-			</Div>
-		</Group>
-	</Panel>
-);
->>>>>>> 1e112d6fc432c351687cc3ff332017ec0e8d7926
 
 Selection.propTypes = {
 	id: PropTypes.string.isRequired,
